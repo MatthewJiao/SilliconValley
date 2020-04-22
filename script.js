89fe44880dce832c1ee3ea772e00e7a2a250aa5d
Matthew Jiao <jiaomatthew222@gmail.com>
2020-04-22 19:14:24 -0400

    anew

--- script.js
+++ script.js
@@ -1,51 +1,57 @@
+// lists for store info 
 var names = []
 var address = []
 var rating = []
 var storeSize = []
 var storeId = []
 
+
 var map
 var request
 var service
 var rad = 5
+
+// starting location
 var lat = 43.888882
 var long = -79.415550
 
 
 
+
+
+// when google maps loads, this function starts 
 function initialize() {
     
-   clearList()
     var center = new google.maps.LatLng(lat,long)
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
-        zoom: 13
+        zoom: 13 
     })
-        request = {
+
+    // request1 with parameter 'supermarket' 
+    request = {
         location: center,
         radius: rad*1000,
         types: ['supermarket']
+    }
         
-        // supermarket maybe with grocery
-        //department_store for costco
-        }
 
+    // request2 with parameter 'costco/walmart' 
+    request2 = {
+        location: center,
+        radius: rad*1000,
+        types: ['department_store']
+    }
     services = new google.maps.places.PlacesService(map)
-    services.nearbySearch(request, callback)
 
-        request2 = {
-            location: center,
-            radius: rad*1000,
-            types: ['department_store']
-        }
-        services.nearbySearch(request2, callback2)
-
-
-
-     
+    services.nearbySearch(request, callback1) // request 1 
+    services.nearbySearch(request2, callback2)  // request 2 
 }
 
-function callback(results, status) {
+// callback functions are called at the very END of the program
+
+// callback1 is to collect 'supermarkets'
+function callback1(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK){
         for(var i = 0; i < results.length; i++){
             createMarker(results[i])
@@ -57,14 +63,10 @@ function callback(results, status) {
            
         }
     }
-  // console.log(storeSize,names)
-    //sortSizes()
-    //console("sizes", )
- //   console.log(results)
- //  display(0) //displays store info in screen
-   addListener() 
+ 
 }
 
+// callback2 is to collect 'costco and walmart'
 function callback2(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK){
         for(var i = 20; i < 20 + results.length; i++){
@@ -74,17 +76,15 @@ function callback2(results, status) {
             rating[i] = results[i-20].rating
             storeSize[i] = results[i-20].user_ratings_total
             storeId[i] = results[i-20].id
-           // console.log(storeId[i])
 
         }
     }
-    //console.log("end")
-  //  deleteRepeat()
     sortSizes()
     display() //displays store info in screen
-    addListener() 
+  //  addListener() 
 }
 
+//adds a marker on map 
 function createMarker(place) {
     var placeLoc = place.geometry.location
     var marker = new google.maps.Marker({
@@ -93,8 +93,14 @@ function createMarker(place) {
     })
 }
 
+
+//when google maps loads, the 'initialize' function is called 
 google.maps.event.addDomListener(window, 'load', initialize)
 
+
+
+
+// output store info into table layout
 function display(){
     for (var i = 0; i < names.length; i++){
         if(names[i]==null){
@@ -102,12 +108,13 @@ function display(){
         }
         cells[i].innerText = `${names[i]}`
         console.log(names[i])
-    //  console.log(storeId[i])
-
 
     }
 }
+
+// add listener for search butten
 function addListener(){
+    //initializeObjects()
     searchForm.addEventListener('submit', e=> {
         e.preventDefault()
         rad = radius.value
@@ -119,8 +126,8 @@ function addListener(){
     })
 }
 
+// sort stores by size
 function sortSizes(){
-   // console.log(storeSize)
     var swapp;
     var n = storeSize.length-1;
     do {
@@ -145,8 +152,6 @@ function sortSizes(){
                address[i] = address[i+1];
                address[i+1] = temp4;
 
-
-
                swapp = true;
             }
         }
@@ -154,18 +159,40 @@ function sortSizes(){
     } while (swapp);
 }
 
-
-function clearList(){
-    names = []
-    address = []
-    rating = []
-    storeSize = []
-    storeId = []
-    
+// to initialize objects in js
+function initializeObjects(){
+    const cells = document.querySelectorAll('.cell')
+    const searchForm = document.getElementById('search-container')
 }
 
-function deleteRepeat(){
-    let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
-  //  console.log(findDuplicates(storeId)) // All duplicates
-}
+
+
+
+
+// auto complete
+var searchInput = 'search_input'
+$(document).ready(function () {
+    var autocomplete
+    autocomplete = new google.maps.places.Autocomplete((document.getElementById(searchInput)), {
+        types: ['geocode'] 
+    })
+    google.maps.event.addDomListener(autocomplete, 'place_changed', function () {
+        var near_place = autocomplete.getPlace()
+        document.getElementById('latitude_input').value = near_place.geometry.location.lat()
+        document.getElementById('longitude_input').value = near_place.geometry.location.lng()
+
+        document.getElementById('latitude_view').innerHTML = near_place.geometry.location.lat()
+        document.getElementById('longitude_view').innerHTML = near_place.geometry.location.lng()
+
+
+    })
+})
+
+//show lat and long
+$(document).on('change','#'+searchInput, function () {
+    document.getElementById('latitude_input').value = ''
+    document.getElementById('longitude_input').value = ''
+    document.getElementById('latitude_view').innerHTML = ''
+    document.getElementById('longitude_view').innerHTML = ''
+})
 
