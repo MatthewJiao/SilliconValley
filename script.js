10c5b6b9e92dd3566cf3ed4780d2419efbb50e2e
Matthew Jiao <jiaomatthew222@gmail.com>
2020-04-22 15:24:02 -0400

    new

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var names = ['']
+var names = []
 var address = []
 var rating = []
+var storeSize = []
+var storeId = []
+
 var map
 var request
 var service
@@ -11,6 +14,7 @@ var long = -79.415550
 
 
 function initialize() {
+   clearList()
     var center = new google.maps.LatLng(lat,long)
     map = new google.maps.Map(document.getElementById('map'), {
         center: center,
@@ -19,13 +23,25 @@ function initialize() {
         request = {
         location: center,
         radius: rad*1000,
-        types: ['grocery_or_supermarket']
-    }
+        types: ['supermarket']
+        
+        // supermarket maybe with grocery
+        //department_store for costco
+        }
 
     services = new google.maps.places.PlacesService(map)
     services.nearbySearch(request, callback)
 
-    removeChildren()    
+        request2 = {
+            location: center,
+            radius: rad*1000,
+            types: ['department_store']
+        }
+        services.nearbySearch(request2, callback2)
+
+
+
+     
 }
 
 function callback(results, status) {
@@ -34,17 +50,38 @@ function callback(results, status) {
             createMarker(results[i])
             names[i] = results[i].name
             address[i] = results[i].vicinity
-           // console.log(results[i].opening_hours)
             rating[i] = results[i].rating
+            storeSize[i] = results[i].user_ratings_total
+            storeId[i] = results[i].id
+           
+        }
+    }
+  // console.log(storeSize,names)
+    //sortSizes()
+    //console("sizes", )
+ //   console.log(results)
+ //  display(0) //displays store info in screen
+   addListener() 
+}
+
+function callback2(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK){
+        for(var i = 20; i < 20 + results.length; i++){
+            createMarker(results[i-20])
+            names[i] = results[i-20].name
+            address[i] = results[i-20].vicinity
+            rating[i] = results[i-20].rating
+            storeSize[i] = results[i-20].user_ratings_total
+            storeId[i] = results[i-20].id
+           // console.log(storeId[i])
 
         }
     }
-    console.log(names,address,rating)
-
-   // console.log('hi')
-    display()
-    addListener()
-
+    //console.log("end")
+    deleteRepeat()
+    sortSizes()
+    display() //displays store info in screen
+    addListener() 
 }
 
 function createMarker(place) {
@@ -58,25 +95,19 @@ function createMarker(place) {
 google.maps.event.addDomListener(window, 'load', initialize)
 
 
-function appendStore(name, address, rating){
-    const messageElement = document.createElement('div')
-    messageElement.innerText = `Store: ${name} \n Location: ${address} \n Rating: ${rating}`
-    storeContainer.appendChild(messageElement) 
-
-}
-
-function removeChildren(){
-    storeContainer.length = 0
-
-}
-
-
 function display(){
     for (var i = 0; i < names.length; i++){
-        appendStore(names[i], address[i], rating[i])
+        if(names[i]==null){
+            break
+        }
+        cells[i].innerText = `${names[i]} ${storeId[i]}`
+     // console.log(names[i])
+    //  console.log(storeId[i])
+
 
     }
 }
+
 
 function addListener(){
     searchForm.addEventListener('submit', e=> {
@@ -84,29 +115,58 @@ function addListener(){
         rad = radius.value
         lat = latitude_input.value
         long = longitude_input.value
-        //showTime()
+        
         initialize()
 
     })
 }
 
-/*
-function showTime() {
-    var center = new google.maps.LatLng(long,lat)
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: center,
-        zoom: 13
-    })
-        request = {
-        location: center,
-        radius: rad*10,
-        types: ['grocery_or_supermarket']
-    }
+function sortSizes(){
+   // console.log(storeSize)
+    var swapp;
+    var n = storeSize.length-1;
+    do {
+        swapp = false;
+        for (var i=0; i < n; i++)
+        {
+            if (storeSize[i] < storeSize[i+1])
+            {
+               var temp = storeSize[i];
+               storeSize[i] = storeSize[i+1];
+               storeSize[i+1] = temp;
 
-    services = new google.maps.places.PlacesService(map)
-    services.nearbySearch(request, callback)
+               var temp2 = names[i];
+               names[i] = names[i+1];
+               names[i+1] = temp2;
 
+               var temp3 = rating[i];
+               rating[i] = rating[i+1];
+               rating[i+1] = temp3;
+
+               var temp4 = address[i];
+               address[i] = address[i+1];
+               address[i+1] = temp4;
+
+
+
+               swapp = true;
+            }
+        }
+        n--;
+    } while (swapp);
+}
+
+
+function clearList(){
+    names = []
+    address = []
+    rating = []
+    storeSize = []
+    storeId = []
     
 }
 
-*/
+function deleteRepeat(){
+    let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) != index)
+  //  console.log(findDuplicates(storeId)) // All duplicates
+}
